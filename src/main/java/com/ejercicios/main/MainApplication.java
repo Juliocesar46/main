69fd99352e05cafa262c7e2e7e2e8e960567c2b0
Juliocesar46 <164976767+Juliocesar46@users.noreply.github.com>
2024-03-26 02:11:43 -0300

    first commit

---
+++ src/main/java/com/ejercicios/main/MainApplication.java
@@ -0,0 +1,22 @@
+package com.ejercicios.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+	public static void main(String[] args) {
+		int edad = 29;
+		String nombreusuario = "luciana";
+		System.out.println(nombreusuario);
+		System.out.print(edad);
+		int producto1 = 1000;
+		int producto2 = 3000;
+		System.out.println("valor final de los productos :"+ sumar (sumarIVA(producto1) , sumarIVA(producto2);
+
+		int monto_cliente = 5500;
+		
+
+	}
+
+}
